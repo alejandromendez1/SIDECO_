1633157ebbf5d083f556c7ffe3940d2933101005
ulises lopez <uliseslopez1112@gmail.com>
2017-09-22 09:19:03 -0300

    agregue admin

--- dimeca/urls.py
+++ dimeca/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 from . import *
 
 urlpatterns = [
+	url(r'^admin/', admin.site.urls),
     url(r'^$', core_views.home, name='home'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
